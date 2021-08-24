8781c4db7490bba270a71d7464e422c539dbf43f
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-08-24 11:08:29 +0300

    fixed stock updating

--- src/reducer/product-reducer.js
+++ src/reducer/product-reducer.js
@@ -7,11 +7,33 @@ export const initialState = {
 
 export const productReducer = (state = initialState, action) => {
   const { type, payload } = action;
-
+  let filteredProducts;
+  let products;
   switch (type) {
     case 'FILTER_PRODUCTS':
-      const filteredProducts = state.products.filter((product) => product.category == payload);
-      const products = state.products;
+      products = state.products;
+      filteredProducts = state.products.filter((product) => product.category == payload);
+
+      return { filteredProducts, products };
+    case 'ADD_TO_CART':
+      products = state.products;
+      filteredProducts = state.filteredProducts.map((product) => {
+        if (product.name == payload) {
+          product.inventoryCount = product.inventoryCount - 1;
+        }
+        return product;
+      });
+
+      return { filteredProducts, products };
+
+    case 'DELETE_FROM_CART':
+      products = state.products;
+      filteredProducts = state.filteredProducts.map((product) => {
+        if (product.name == payload) {
+          product.inventoryCount = product.inventoryCount + 1;
+        }
+        return product;
+      });
 
       return { filteredProducts, products };
 
